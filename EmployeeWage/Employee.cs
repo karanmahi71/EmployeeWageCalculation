8bd1ccb643533e3b043fef5572cd007c58146db4
karanmahi71 <karankasbe7171@gmail.com>
2022-08-23 22:20:22 +0530

    employee attendance

---
+++ EmployeeWage/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Employee
+    {
+        int isPresent = 1;
+        Random random = new Random();
+        public void Attendance()
+        {
+            int result = random.Next(0, 2);
+            if (result == isPresent)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+   internal class Program
+    {
+        public void Main(string[] args)
+        {
+         Employee empname = new Employee();
+            empname.Attendance();
+        }
+    }
+}
